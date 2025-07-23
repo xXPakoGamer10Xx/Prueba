f442dc8b7be5427cb1573a234b8225909d2c475d
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2025-07-23 09:17:58 -0600

    OD/C - ME (DELETE)

--- app/Livewire/Odontologia/Consultorio/MaterialesExternosTable.php
+++ app/Livewire/Odontologia/Consultorio/MaterialesExternosTable.php
@@ -3,20 +3,50 @@
 namespace App\Livewire\Odontologia\Consultorio;
 
 use Livewire\Component;
-use Livewire\WithPagination; // Importar el trait de paginación
-use App\Models\Odontologia\MaterialesExternos; // Importar el modelo de Materiales Externos
+use Livewire\WithPagination;
+use App\Models\Odontologia\MaterialesExternos;
 
 class MaterialesExternosTable extends Component
 {
     use WithPagination;
 
     public $search = ''; // Propiedad para la búsqueda, si se desea implementar
+    public $materialToDeleteId; // Propiedad para almacenar el ID del material a eliminar
 
     // Opcional: Para resetear la paginación cuando cambia la búsqueda
     public function updatingSearch()
     {
         $this->resetPage();
     }
+
+    /**
+     * Establece el ID del material a eliminar y muestra la modal de confirmación.
+     *
+     * @param int $id El ID del material externo a eliminar.
+     * @return void
+     */
+    public function confirmDelete($id)
+    {
+        $this->materialToDeleteId = $id;
+        $this->dispatch('open-modal', 'deleteMaterialModal'); // Despacha un evento para 
+    }
+
+    /**
+     * Elimina el material externo de la base de datos.
+     *
+     * @return void
+     */
+    public function deleteMaterial()
+    {
+        if ($this->materialToDeleteId) {
+            MaterialesExternos::find($this->materialToDeleteId)->delete();
+            $this->materialToDeleteId = null; // Limpiar el ID después de la eliminación
+            $this->dispatch('close-modal', 'deleteMaterialModal'); // Despacha un evento para cerrar la modal
+            session()->flash('message', 'Material externo eliminado exitosamente.'); // Mensaje de éxito
+            $this->dispatch('$refresh'); // Recargar la tabla
+        }
+    }
+
 
     public function render()
     {
--- resources/views/livewire/odontologia/consultorio/materiales-externos-table.blade.php
+++ resources/views/livewire/odontologia/consultorio/materiales-externos-table.blade.php
@@ -1,41 +1,48 @@
 <div>
-  @if($materialesExternos->isEmpty())
-      <p class="text-center">No se encontraron registros</p>
-  @else
-    <div class="table-responsive">
-      <table class="table table-bordered table-hover table-striped text-center">
-          <thead class="bg-cafe text-white table-dark">
-              <tr>
-                  <th scope="col" class="w-[8rem]">ID</th>
-                  <th scope="col">Descripción</th>
-                  <th scope="col" class="w-[8rem]">Cantidad</th>
-                  <th scope="col" class="w-[8rem]">Acciones</th>
-              </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
-              @foreach ($materialesExternos as $material)
-                  <tr>
-                      <td>{{ $material->id_material }}</td>
-                      <td><input class="w-full" type="text" value="{{ $material->descripcion }}"></td>
-                      <td><input class="w-full" type="number" min="0" value="{{ $material->cantidad }}"></td>
-                      <td>
-                        <i
-                            class='fa-solid fa-trash-can cursor-pointer'
+    @if($materialesExternos->isEmpty())
+        <p class="text-center">No se encontraron registros</p>
+    @else
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-striped text-center">
+                <thead class="bg-cafe text-white table-dark">
+                    <tr>
+                        <th scope="col" class="w-[8rem]">ID</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col" class="w-[8rem]">Cantidad</th>
+                        <th scope="col" class="w-[8rem]">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($materialesExternos as $material)
+                        <tr>
+                            <td>{{ $material->id_material }}</td>
+                            <td><input class="w-full" type="text" value="{{ $material->descripcion }}"></td>
+                            <td><input class="w-full" type="number" min="0" value="{{ $material->cantidad }}"></td>
+                            <td>
+                                <i
+                                    class='fa-solid fa-trash-can cursor-pointer'
+                                    wire:click="confirmDelete({{ $material->id_material }})"
+                                    title="Eliminar registro"
+                                ></i>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    @endif
 
-                            title="Eliminar registro"
-                        ></i>
-                      </td>
-                  </tr>
-              @endforeach
-          </tbody>
-      </table>
+    {{-- Paginación --}}
+    <div class="mt-4">
+        {{ $materialesExternos->links() }}
     </div>
-  @endif
 
-  <x-modals.delete-confirmation
-    modalId=""
-    formId=""
-    wireModel=""
-    confirmAction=""
-  />
+    {{-- Modal de confirmación de eliminación --}}
+    <x-modals.delete-confirmation
+        modalId="deleteMaterialModal"
+        formId="deleteMaterialForm"
+        wireModel="showDeleteConfirmationModal"
+        confirmAction="deleteMaterial"
+    >
+    </x-modals.delete-confirmation>
 </div>
