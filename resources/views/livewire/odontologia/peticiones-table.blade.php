fa46a0c07512312d6c5558e1d535cbd13374c0ce
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2025-07-28 09:36:40 -0600

    OD - Peticiones feat: estilos condicionales para tabla

--- resources/views/livewire/odontologia/peticiones-table.blade.php
+++ resources/views/livewire/odontologia/peticiones-table.blade.php
@@ -6,29 +6,59 @@
             <table class="table table-bordered table-hover table-striped text-center">
                 <thead class="bg-cafe text-white table-dark">
                     <tr>
-                        <th scope="col">ID <br>(Almcen)</th>
+                        <th scope="col">ID<br>(Almacén)</th>
                         <th scope="col">Clave</th>
                         <th scope="col">Descripción</th>
-                        <th scope="col">Cantidad Solicitada</th>
-                        <th scope="col">Cantidad Autorizada</th>
+                        <th scope="col">Cantidad<br>Solicitada</th>
+                        <th scope="col">Cantidad<br>Autorizada</th>
                         <th scope="col">Estado</th>
-                        <th scope="col">Fecha Solicitada</th>
-                        <th scope="col">Fecha Entrega</th>
+                        <th scope="col">Fecha<br>Solicitada</th>
+                        <th scope="col">Fecha<br>Entrega</th>
                         <th scope="col">Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($pedidos as $pedido)
-                        <tr>
+                        <tr
+                            @if($pedido->estado_pedido == 'Cancelado')
+                            class="opacity-50"
+                            @endif
+                        >
                             <td>{{ $pedido->id_insumo_almacen_fk }}</td>
                             {{-- Acceder a las propiedades del insumo a través de la relación --}}
                             <td>{{ $pedido->almacenItem->insumo->clave }}</td>
                             <td>{{ $pedido->almacenItem->insumo->descripcion }}</td>
                             <td>{{ $pedido->cantidad_solicitada }}</td>
-                            <td>{{ $pedido->cantidad_autorizada }}</td>
+                            <td>
+                                @if($pedido->estado_pedido == 'Cancelado')
+                                    Cancelado
+                                @else
+                                    {{ $pedido->cantidad_autorizada }}
+                                @endif
+                            </td>
                             <td>{{ $pedido->estado_pedido }}</td>
                             <td>{{ $pedido->fecha_pedido ? $pedido->fecha_pedido->format('d-m-Y') : 'Sin fecha' }}</td>
-                            <td>{{ $pedido->fecha_entrega ? $pedido->fecha_entrega->format('d-m-Y') : 'Pendiente' }}</td>
+                            <td>
+                                @if($pedido->estado_pedido == 'Cancelado')
+                                    Cancelado
+                                @else
+                                    {{ $pedido->fecha_entrega ? $pedido->fecha_entrega->format('d-m-Y') : 'Pendiente' }}
+                                @endif
+                        </td>
+                            <td class='d-flex justify-content-evenly'>
+                                <button 
+                                    type='button'
+                                    class='btn-cancelar text-black border-0 bg-transparent'
+                                    title="Cancelar pedido"
+                                    data-bs-toggle='modal' 
+                                    data-bs-target='#modalCancelarPedido'
+                                    @if($pedido->estado_pedido == 'Cancelado')
+                                        disabled
+                                    @endif
+                                >
+                                    <i class='fa-solid fa-xmark'></i>
+                                </button>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
